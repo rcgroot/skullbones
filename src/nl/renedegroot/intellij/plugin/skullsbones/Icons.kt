5165efaccd571f580afedc0413329f7844d9cfad
Rene de Groot <rcgroot@gmail.com>
2018-07-11 20:58:10 +0200

    Change icons, now including a dark theme

--- src/nl/renedegroot/intellij/plugin/skullsbones/Icons.kt
+++ src/nl/renedegroot/intellij/plugin/skullsbones/Icons.kt
@@ -3,5 +3,5 @@ package nl.renedegroot.intellij.plugin.skullsbones
 import com.intellij.openapi.util.IconLoader
 
 object Icons {
-    val skullBones = IconLoader.getIcon("/gutter/skull-on-top-of-a-cross-of-bones-in-a-rounded-square.png")
+    val skullBones = IconLoader.getIcon("/gutter/death-icon.png")
 }
